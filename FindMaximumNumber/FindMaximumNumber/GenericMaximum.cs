29f4c71e53f6bf426b5edf445be2417813002b34
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-29 22:17:06 +0530

    Finding the maximum value

---
+++ FindMaximumNumber/FindMaximumNumber/GenericMaximum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        //This method is created for initialize the constructor of instance variables.
+        public GenericMaximum(T[] valuee)
+        {
+            this.value = valuee;
+        }
+        //This method is created for sorting the values.
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        //This method is created for finding max for given type.
+        public T MaxValue(T[] values)
+        {
+            var Sorted_value = Sort(values);
+            return Sorted_value[^1];
+        }
+        //This method is created calling maxvalue method
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        //Thid method is printing maximum generic value.
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Max Value is : " + max);
+        }
+    }
+}
+internal class GenericMaximum
+    {
+    }
+}
--- FindMaximumNumber/FindMaximumNumber/Program.cs
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -6,15 +6,27 @@
         {
             Console.WriteLine("Welcome to Program to Find Maximum Number");
 
-            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
-            int integerOutput = maximumNumberCheck.MaximumIntegerNumber(99, 110, 150);
-            Console.WriteLine(integerOutput);
+            //MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            //int integerOutput = maximumNumberCheck.MaximumIntegerNumber(99, 110, 150);
+            //Console.WriteLine(integerOutput);
 
-            double doubleOutput = maximumNumberCheck.MaximumFloatNumber(111.1567, 234.5466, 668.9890);
-            Console.WriteLine(doubleOutput);
+            //double doubleOutput = maximumNumberCheck.MaximumFloatNumber(111.1567, 234.5466, 668.9890);
+            //Console.WriteLine(doubleOutput);
 
-            string stringOutput = maximumNumberCheck.MaximumStringNumber("22", "33", "44");
-            Console.WriteLine(stringOutput);
+            //string stringOutput = maximumNumberCheck.MaximumStringNumber("22", "33", "44");
+            //Console.WriteLine(stringOutput);
+
+            int[] intArray = { 12, 45, 67, 20, 90, 47 };
+            GenericMaximum<int> genericMaximum = new GenericMaximum<int>(intArray);
+            genericMaximum.PrintMaxValue();
+
+            double[] doubleArray = { 120.12, 340.45, 45.65, 680.43, 765.52 };
+            GenericMaximum<double> genericMaximum1 = new GenericMaximum<double>(doubleArray);
+            genericMaximum1.PrintMaxValue();
+
+            string[] strings = { "44", "22", "66", "555", "999" };
+            GenericMaximum<string> genericMaximum2 = new GenericMaximum<string>(strings);
+            genericMaximum2.PrintMaxValue();
         }
     }
 }
