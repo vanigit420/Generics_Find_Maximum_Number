c582aae287de9da4558c3470efd7cb99949416d6
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-29 21:57:59 +0530

    Finding the maximum integer number

---
+++ FindMaximumNumber/FindMaximumNumber/MaximumNumberCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class MaximumNumberCheck
+    {
+        /// <summary>
+        /// This is method is created for Maximum Integer Number.
+        /// </summary>
+        public int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("first value,second value, & third value are same");
+        }
+    }
+}
---
+++ FindMaximumNumber/FindMaximumNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FindMaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Program to Find Maximum Number");
+
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            int integerOutput = maximumNumberCheck.MaximumIntegerNumber(99, 110, 150);
+            Console.WriteLine(integerOutput);
+        }
+    }
+}
